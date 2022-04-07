4b1d434eda02d355e3b7bf3eb98ee6869599115f
Ukalus <luhof50@gmail.com>
2022-04-07 15:30:17 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Pokeimage from "./Pokeimage.svelte"
+	let searchname;
+	let details;
+	let pokelistpromise = catchpokemon("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151");
+	async function catchpokemon(api_link){
+		const res = await fetch(api_link);
+		const data = await res.json();
+
+		if(res.ok){
+			
+			return(data);
+		}
+		else{
+			throw new Error(data);
+		}
+		
+	}
+
+</script>
+
+<main>
+	<p>type in your pokemons name</p>
+	<input bind:value={searchname}>
+	<button on:click={() => (catchpokemon())}></button>
+
+	{#await pokelistpromise}
+		<p>... loading</p>
+	{:then pokelist}
+		{#each pokelist.results as pokemon}
+		{#if pokemon.name == searchname}
+		<p>{pokemon.name}</p>
+		<Pokeimage link="{pokemon.url}"></Pokeimage>
+		
+		{/if}
+		{/each}
+	{:catch error}
+		<p>error</p>
+	{/await}
+	
+
+
+</main>
+
---
+++ src/Pokeimage.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let link;
+    let pokemonpromise = current_pokemon(link);
+    async function current_pokemon(api_link){
+		const res = await fetch(api_link);
+		const pokemon_data = await res.json();
+		
+
+		if(res.ok){
+			
+			return(pokemon_data);
+		}
+		else{
+			throw new Error(pokemon_data);
+		}
+		
+	}
+</script>
+
+
+
+{#await pokemonpromise }
+    ..loading
+{:then pokemondetails} 
+<img src="{pokemondetails.sprites.front_default}" alt="error">
+<img src="{pokemondetails.sprites.front_shiny}" alt="error">
+
+{/await}
