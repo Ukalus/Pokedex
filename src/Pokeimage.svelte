16c753b62bdc9d2e1a1c3f75db3d6b7369c848b3
Ukalus <luhof50@gmail.com>
2022-04-07 16:01:42 +0200

    arcordion added

--- src/Pokeimage.svelte
+++ src/Pokeimage.svelte
@@ -22,7 +22,32 @@
 {#await pokemonpromise }
     ..loading
 {:then pokemondetails} 
-<img src="{pokemondetails.sprites.front_default}" alt="error">
-<img src="{pokemondetails.sprites.front_shiny}" alt="error">
+<div class="accordion" id="accordionPanelsStayOpenExample">
+	<div class="accordion-item">
+	  <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+		<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+		  {JSON.stringify(pokemondetails.sprite.key)}
+		</button>
+	  </h2>
+	  <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+		<div class="accordion-body">
+			<img src="{pokemondetails.sprites.front_default}" alt="error">
+		</div>
+	  </div>
+	</div>
+	<div class="accordion-item">
+	  <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+		<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+		  Accordion Item #2
+		</button>
+	  </h2>
+	  <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+		<div class="accordion-body">
+			<img src="{pokemondetails.sprites.front_shiny}" alt="error">
+		</div>
+	  </div>
+	</div>
+  </div>
+
 
 {/await}
